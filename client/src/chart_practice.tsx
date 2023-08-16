e6a04ae05b12dbd1b5650dd4d0c7de9e2fe261fb
kegcheck <65734212+kegcheck@users.noreply.github.com>
2023-08-16 18:47:29 -0500

    add submit answer

--- client/src/chart_practice.tsx
+++ client/src/chart_practice.tsx
@@ -10,105 +10,113 @@ const ChartPractice: React.FC = () => {
         userAnswer: number;
         hasAnswered: boolean;
         row: number;
+        correct: boolean;
     }
 
     interface AnswerKey {
         [key: string]: AnswerData;
-      }
+    }
     
     const initialAnswerKey: AnswerKey = {
-        "A-A": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0},
-        "A-K": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0},
-        "K-Q": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0},
-        "Q-J": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0},
-        "J-T": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0},
-        "T-9": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0},
-        "9-8": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 0},
-        "8-7": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 0},
-        "7-6": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 0},
-        "6-5": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 0},
-        "5-4": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 0},
-        "4-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 0},
-        "3-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 0},
-        "K-K": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 1},
-        "A-Q": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 1},
-        "K-J": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 1},
-        "Q-T": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 1},
-        "J-9": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 1},
-        "T-8": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 1},
-        "9-7": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 1},
-        "8-6": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 1},
-        "7-5": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 1},
-        "6-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 1},
-        "5-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 1},
-        "4-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 1},
-        "Q-Q": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 2},
-        "A-J": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 2},
-        "K-T": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 2},
-        "Q-9": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 2},
-        "J-8": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 2},
-        "T-7": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 2},
-        "9-6": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 2},
-        "8-5": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 2},
-        "7-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 2},
-        "6-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 2},
-        "5-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 2},
-        "J-J": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 3},
-        "A-T": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 3},
-        "K-9": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 3},
-        "Q-8": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 3},
-        "J-7": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 3},
-        "T-6": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 3},
-        "9-5": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 3},
-        "8-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 3},
-        "7-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 3},
-        "6-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 3},
-        "T-T": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 4},
-        "A-9": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 4},
-        "K-8": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 4},
-        "Q-7": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4},
-        "J-6": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4},
-        "T-5": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4},
-        "9-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4},
-        "8-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4},
-        "7-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4},
-        "9-9": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 5},
-        "A-8": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 5},
-        "K-7": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 5},
-        "Q-6": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 5},
-        "J-5": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 5},
-        "T-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 5},
-        "9-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 5},
-        "8-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 5},
-        "8-8": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 6},
-        "A-7": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 6},
-        "K-6": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 6},
-        "Q-5": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 6},
-        "J-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 6},
-        "T-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 6},
-        "9-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 6},
-        "7-7": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 7},
-        "A-6": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 7},
-        "K-5": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 7},
-        "Q-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 7},
-        "J-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 7},
-        "T-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 7},
-        "6-6": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 8},
-        "A-5": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 8},
-        "K-4": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 8},
-        "Q-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 8},
-        "J-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 8},
-        "5-5": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 9},
-        "A-4": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 9},
-        "K-3": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 9},
-        "Q-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 9},
-        "4-4": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 10},
-        "A-3": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 10},
-        "K-2": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 10},
-        "3-3": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 11},
-        "A-2": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 11},
-        "2-2": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 12}
+        "A-A": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "A-K": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "K-Q": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "Q-J": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "J-T": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "T-9": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "9-8": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "8-7": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "7-6": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "6-5": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "5-4": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "4-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "3-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 0, correct: false},
+        "K-K": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "A-Q": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "K-J": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "Q-T": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "J-9": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "T-8": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "9-7": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "8-6": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "7-5": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "6-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "5-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "4-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 1, correct: false},
+        "Q-Q": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 2, correct: false},
+        "A-J": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 2, correct: false},
+        "K-T": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 2, correct: false},
+        "Q-9": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 2, correct: false},
+        "J-8": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 2, correct: false},
+        "T-7": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 2, correct: false},
+        "9-6": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 2, correct: false},
+        "8-5": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 2, correct: false},
+        "7-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 2, correct: false},
+        "6-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 2, correct: false},
+        "5-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 2, correct: false},
+        "J-J": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 3, correct: false},
+        "A-T": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 3, correct: false},
+        "K-9": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 3, correct: false},
+        "Q-8": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 3, correct: false},
+        "J-7": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 3, correct: false},
+        "T-6": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 3, correct: false},
+        "9-5": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 3, correct: false},
+        "8-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 3, correct: false},
+        "7-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 3, correct: false},
+        "6-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 3, correct: false},
+        "T-T": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 4, correct: false},
+        "A-9": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 4, correct: false},
+        "K-8": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 4, correct: false},
+        "Q-7": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4, correct: false},
+        "J-6": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4, correct: false},
+        "T-5": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4, correct: false},
+        "9-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4, correct: false},
+        "8-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4, correct: false},
+        "7-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 4, correct: false},
+        "9-9": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 5, correct: false},
+        "A-8": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 5, correct: false},
+        "K-7": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 5, correct: false},
+        "Q-6": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 5, correct: false},
+        "J-5": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 5, correct: false},
+        "T-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 5, correct: false},
+        "9-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 5, correct: false},
+        "8-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 5, correct: false},
+        "8-8": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 6, correct: false},
+        "A-7": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 6, correct: false},
+        "K-6": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 6, correct: false},
+        "Q-5": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 6, correct: false},
+        "J-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 6, correct: false},
+        "T-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 6, correct: false},
+        "9-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 6, correct: false},
+        "7-7": {correctAnswer: 0, userAnswer: -1, hasAnswered: false, row: 7, correct: false},
+        "A-6": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 7, correct: false},
+        "K-5": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 7, correct: false},
+        "Q-4": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 7, correct: false},
+        "J-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 7, correct: false},
+        "T-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 7, correct: false},
+        "6-6": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 8, correct: false},
+        "A-5": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 8, correct: false},
+        "K-4": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 8, correct: false},
+        "Q-3": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 8, correct: false},
+        "J-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 8, correct: false},
+        "5-5": {correctAnswer: 1, userAnswer: -1, hasAnswered: false, row: 9, correct: false},
+        "A-4": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 9, correct: false},
+        "K-3": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 9, correct: false},
+        "Q-2": {correctAnswer: 3, userAnswer: -1, hasAnswered: false, row: 9, correct: false},
+        "4-4": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 10, correct: false},
+        "A-3": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 10, correct: false},
+        "K-2": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 10, correct: false},
+        "3-3": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 11, correct: false},
+        "A-2": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 11, correct: false},
+        "2-2": {correctAnswer: 2, userAnswer: -1, hasAnswered: false, row: 12, correct: false}
     };
+
+    let testData = {
+        score_percent: 0,
+        score_percent_string: '',
+        score_correct: 0,
+        time: 0
+    }
 
     const [answerKey, setAnswerKey] = useState<AnswerKey>(initialAnswerKey);
       
@@ -130,10 +138,25 @@ const ChartPractice: React.FC = () => {
 
           setColoredAnswer(event);
           
+          
     }
 
-    const saveTestAnswers = () => {
-
+    const handleSubmitAnswers = () => {
+        let correct = 0;
+        let total = 0;
+        Object.values(answerKey).forEach((answer: AnswerData) => {
+            if (answer.correctAnswer === answer.userAnswer) {
+                answer.correct = true;
+                correct++;
+            } else {
+                answer.correct = false;
+            }
+            total++;
+        });
+        testData.score_percent_string = `${Math.round((correct / total) * 100)}%`
+        testData.score_percent = correct / total;
+        testData.score_correct = correct;
+        console.log(testData)
     }
 
     const setColoredAnswer = (event:any) => {
@@ -142,6 +165,25 @@ const ChartPractice: React.FC = () => {
         clickedElement.classList.remove('always', 'mid', 'late');
         clickedElement.classList.add(colors[currentlySelectingFor])
     }
+
+    const [chartCleared, setChartCleared] = useState(false); // State variable
+
+    const clearChart = () => {
+      const tableCells = document.querySelectorAll('td');
+      tableCells.forEach(cell => {
+        cell.classList.remove('always', 'mid', 'late');
+      });
+
+      Object.values(answerKey).forEach((answer: AnswerData) => {
+        answer.userAnswer = -1;
+        answer.hasAnswered = false
+        answer.correct = false;
+      });
+  
+      setChartCleared(true); // Call the setter function to update state
+    };
+
+
 
     let currentRow = -1;
     let totalRows = Math.max(...Object.values(answerKey).map((item) => item.row)) + 1;
@@ -186,6 +228,8 @@ const ChartPractice: React.FC = () => {
                         {selectionOptions[3]}
                     </button>
                 </div>
+                <button className='btn-secondary' onClick={()=>handleSubmitAnswers()}>Submit Answers</button>
+                <button onClick={()=>clearChart()}>Clear Answers</button>
             </div>
             <table>
             <tbody>
@@ -195,7 +239,6 @@ const ChartPractice: React.FC = () => {
                         <tr key={rowIndex}>
                             {Object.keys(answerKey).map((answer) => {
                                 if (answerKey[answer as keyof typeof answerKey].row === currentRow) {
-                                    console.log("Rows: ", currentRow);
 
                                     return (
                                         <td
